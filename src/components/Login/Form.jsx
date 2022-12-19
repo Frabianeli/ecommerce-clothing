ec4620d826b9f90a2c0d07ed275f1b5b06a04de1
rene <frabianeli.crypto>
2022-12-19 18:42:26 -0500

    login

--- src/components/Login/Form.jsx
+++ src/components/Login/Form.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 import { useForm, useWatch } from 'react-hook-form'
 import { NavLink } from 'react-router-dom'
 import './style/form.css'
 
 const Form = () => {
+
+  const [createUser, setCreateUser] = useState(false)
 
   const {register, handleSubmit, reset} = useForm()
 
@@ -20,13 +23,40 @@ const Form = () => {
     }
     reset()
   }
+
+  const submitRegister = (data) =>{
+    
+    const userCreate = {
+      name: data.name,
+      email: data.email,
+      password: data.password
+    }
+    if(data.name.length && data.email.length && data.password.length > 6){
+      alert(JSON.stringify(userCreate))
+    } else {
+      alert('Minimo 6 caracteres')
+    }
+    reset()
+  }
+
+  const btnCreateUser = () => setCreateUser(!createUser)
+
   return (
     <article className='login'>
       <div className='login__head'>
         <i className="fa-solid fa-user"></i>
-        <h2 className='login__head__title'>Ingresar</h2>
+        <h2 className='login__head__title'>
+          {createUser ? 'Crear usuario' : 'Ingresar'}
+        </h2>
       </div>
       <form onSubmit={handleSubmit(submit)} className='form__login'>
+        {
+          createUser &&
+          <>
+            <label htmlFor="name">nombre:</label>
+            <input type="email" id='name' {...register("name")}/>
+          </>
+        }
         <label htmlFor="email">email:</label>
         <input type="email" id='email' {...register("email")}/>
         <label htmlFor="password">password:</label>
@@ -34,10 +64,16 @@ const Form = () => {
         <button className='form__login__submit'>Enviar</button>
       </form>
       <div className='login__create-user'>
-        <p>No tengo cuenta: </p>
-        <NavLink to={'/register'}>
-            crear cuenta
-        </NavLink>
+        <p>
+          {
+            !createUser ? 'No tengo cuenta: ' : 'Tengo una cuenta: '
+          }
+        </p>
+        <p className='login__create-user-btn' onClick={btnCreateUser}>
+          {
+            !createUser ? 'crear cuenta' : 'iniciar sesión'
+          }
+        </p>
       </div>
       <div className='login__fb'>
         <i className="fa-brands fa-facebook"></i>
@@ -45,7 +81,7 @@ const Form = () => {
       </div>
       <div className='login__google'>
         <i className="fa-brands fa-google"></i>
-        <span>Continuar con <b>gogle</b></span>
+        <span>Continuar con <b>google</b></span>
       </div>
     </article>
   )
